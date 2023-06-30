182d57c2872cba2efb81712b8e24a9d8b357bd37
2046 <2046@ufo.codes>
2023-06-30 13:27:54 +0800

    optimize: add website dialog logic

--- web/assets/js/add.js
+++ web/assets/js/add.js
@@ -1,16 +1,20 @@
 (function() {
   let step = 1
-  let link = ''
-  let name = ''
-  let icon = ''
-  let desc = ''
-  let category = ''
+  let params = {
+    link: '',
+    name: '',
+    icon: '',
+    desc: '',
+    category: ''
+  }
+
   const addElement = utils.$('.J_add')
   const addDialogElement = utils.$('.J_add_dialog')
   const addDialogLinkElement = utils.$('.J_add_link')
   const addDialogNameElement = utils.$('.J_add_name')
   const addDialogIconElement = utils.$('.J_add_icon')
   const addDialogSubmitElement = utils.$('.J_add_submit')
+  const addDialogBoxElement = utils.$('.J_add_dialog .box')
   const addDialogCategoryElement = utils.$('.J_add_category')
   const addDialogCloseElement = utils.$('.J_add_dialog .close')
   const addDialogDescriptionElement = utils.$('.J_add_description')
@@ -21,55 +25,61 @@
     refreshSubmitElementState()
   })
 
+  addDialogBoxElement.addEventListener('click', function(e) {
+    e.stopPropagation()
+  })
+
   addDialogCloseElement.addEventListener('click', function() {
-    step = 1
-    addDialogLinkElement.value = ''
-    addDialogNameElement.value = ''
-    addDialogIconElement.value = ''
-    addDialogCategoryElement.value = ''
-    addDialogDescriptionElement.value = ''
     addDialogElement.classList.remove('open')
-    addDialogSubmitElement.textContent = 'Parse'
-    addDialogNameElement.parentElement.classList.add('hidden')
-    addDialogIconElement.parentElement.classList.add('hidden')
-    addDialogCategoryElement.parentElement.classList.add('hidden')
-    addDialogDescriptionElement.parentElement.classList.add('hidden')
+
+    reset()
+  })
+
+  addDialogElement.addEventListener('click', function() {
+    addDialogElement.classList.remove('open')
+
+    reset()
   })
 
   addDialogLinkElement.addEventListener('input', function() {
-    link = addDialogLinkElement.value.trim()
+    params.link = addDialogLinkElement.value.trim()
+
     refreshSubmitElementState()
   })
 
   addDialogNameElement.addEventListener('input', function() {
-    name = addDialogNameElement.value.trim()
+    params.name = addDialogNameElement.value.trim()
+
     refreshSubmitElementState()
   })
-  
+
   addDialogIconElement.addEventListener('input', function() {
-    icon = addDialogIconElement.value.trim()
+    params.icon = addDialogIconElement.value.trim()
+
     refreshSubmitElementState()
   })
 
   addDialogDescriptionElement.addEventListener('input', function() {
-    desc = addDialogDescriptionElement.value.trim()
+    params.desc = addDialogDescriptionElement.value.trim()
   })
 
   addDialogCategoryElement.addEventListener('input', function() {
-    category = addDialogCategoryElement.value.trim()
+    params.category = addDialogCategoryElement.value.trim()
   })
 
   addDialogSubmitElement.addEventListener('click', function() {
-    if (step === 1 && link) {
+    if (step === 1 && params.link) {
       addDialogSubmitElement.textContent = 'Parseing'
       addDialogSubmitElement.setAttribute('disabled', 'disabled')
 
-      utils.request('POST', '/api/extract', { link, token })
-        .then(res => {
+      return utils.request('POST', '/api/extract', { link: params.link, token: VARS.token })
+        .then(function (res) {
           step = 2
-          name = addDialogNameElement.value = res.name || ''
-          icon = addDialogIconElement.value = res.icon || ''
-          desc = addDialogDescriptionElement.value = res.desc || ''
+          params.name = addDialogNameElement.value = res.name || ''
+          params.icon = addDialogIconElement.value = res.icon || ''
+          params.desc = addDialogDescriptionElement.value = res.desc || ''
+
+          addDialogElement.querySelector('.box-body').classList.remove('off')
 
           addDialogNameElement.parentElement.classList.remove('hidden')
           addDialogIconElement.parentElement.classList.remove('hidden')
@@ -79,58 +89,59 @@
           addDialogSubmitElement.textContent = 'Submit'
           addDialogSubmitElement.removeAttribute('disabled')
         })
-        .catch(err => {
+        .catch(function(err) {
           utils.toast(err.message)
           addDialogSubmitElement.textContent = 'Parse'
           addDialogSubmitElement.removeAttribute('disabled')
         })
-
-      return
     }
 
-    if (step === 2 && link && name && icon) {
+    if (step === 2 && params.link && params.name && params.icon) {
       addDialogSubmitElement.setAttribute('disabled', 'disabled')
 
-      utils.request('POST', '/api/add', { sheetId, token, link, name, icon, desc, category })
-        .then(res => {
-          step = 1
-          addDialogLinkElement.value = ''
-          addDialogNameElement.value = ''
-          addDialogIconElement.value = ''
-          addDialogCategoryElement.value = ''
-          addDialogDescriptionElement.value = ''
+      return utils.request('POST', '/api/add', Object.assign({}, params, { token: VARS.token, sheetId: VARS.sheetId }))
+        .then(function() {
+          reset()
+
           addDialogElement.classList.remove('open')
-          addDialogSubmitElement.textContent = 'Parse'
-          addDialogNameElement.parentElement.classList.add('hidden')
-          addDialogIconElement.parentElement.classList.add('hidden')
-          addDialogCategoryElement.parentElement.classList.add('hidden')
-          addDialogDescriptionElement.parentElement.classList.add('hidden')
           addDialogSubmitElement.removeAttribute('disabled')
         })
-        .catch(err => {
+        .catch(function(err) {
           utils.toast(err.message)
           addDialogSubmitElement.removeAttribute('disabled')
         })
     }
   })
 
-  function refreshSubmitElementState() {
-    if (step === 1) {
-      if (link) {
-        addDialogSubmitElement.removeAttribute('disabled')
-      } else {
-        addDialogSubmitElement.setAttribute('disabled', 'disabled')
-      }
-
-      return
+  function reset() {
+    step = 1
+    params = {
+      link: '',
+      name: '',
+      icon: '',
+      desc: '',
+      category: ''
     }
+    addDialogLinkElement.value = ''
+    addDialogNameElement.value = ''
+    addDialogIconElement.value = ''
+    addDialogCategoryElement.value = ''
+    addDialogDescriptionElement.value = ''
+    addDialogSubmitElement.textContent = 'Parse'
+    addDialogNameElement.parentElement.classList.add('hidden')
+    addDialogIconElement.parentElement.classList.add('hidden')
+    addDialogCategoryElement.parentElement.classList.add('hidden')
+    addDialogDescriptionElement.parentElement.classList.add('hidden')
+    addDialogElement.querySelector('.box-body').classList.add('off')
+  }
 
-    if (step === 2) {
-      if (link && name && icon) {
-        addDialogSubmitElement.removeAttribute('disabled')
-      } else {
-        addDialogSubmitElement.setAttribute('disabled', 'disabled')
-      }
+  function refreshSubmitElementState() {
+    if (step === 1 && params.link) {
+      addDialogSubmitElement.removeAttribute('disabled')
+    } else if (step === 2 && params.link && params.name && params.icon) {
+      addDialogSubmitElement.removeAttribute('disabled')
+    } else {
+      addDialogSubmitElement.setAttribute('disabled', 'disabled')
     }
   }
 })();
